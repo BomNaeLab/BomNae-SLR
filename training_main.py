82b75ee81fb047067c1950eea5690b7d298b8fd3
noobcoconut <jerryterryharry@gmail.com>
2024-10-03 14:03:35 +0900

    removed training_main.py as it is not required

--- training_main.py
+++
@@ -1,9 +0,0 @@
-import preprocess as prep
-import model
-
-# preprocess / load preprocessed data (preprocess.py)
-# mini batching
-
-# load model(can quickly change model by changing imported model)
-# train / log
-# generate checkpoints
